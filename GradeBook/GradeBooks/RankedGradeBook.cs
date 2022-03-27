8d42733e47c55023879ba2d88a55e2ed54b2da98
AnnaBala-WSEI <anna.bala@microsoft.wsei.edu.pl>
2022-03-27 21:56:03 +0200

    Override RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,5 +16,17 @@ namespace GradeBook.GradeBooks
         }
 
 
+        public override void CalculateStatistics()
+        { 
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            } else
+            {
+                base.CalculateStatistics();
+            }
+            
+        }
+
     }
 }
