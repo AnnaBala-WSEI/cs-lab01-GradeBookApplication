fc35c0e1bdbd8ee439b951a5b438a7e0b801b20d
AnnaBala-WSEI <anna.bala@microsoft.wsei.edu.pl>
2022-03-27 20:31:07 +0200

    Create StandardGradeBook and RankedGradeBook classes

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,20 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook 
+    {
+        public new string Name { get; set; }
+
+        public RankedGradeBook(string name) : base(name)
+        {
+            Name = name;
+            Type = GradeBookType.Ranked;
+        }
+
+
+    }
+}
