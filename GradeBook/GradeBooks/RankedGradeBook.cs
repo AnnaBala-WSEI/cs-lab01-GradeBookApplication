11f2bad570b27f495c76b88b759ede911f5941e2
AnnaBala-WSEI <anna.bala@microsoft.wsei.edu.pl>
2022-04-01 22:42:31 +0200

    Make RankedGradeBook and StandardGradeBook classes public

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class RankedGradeBook : BaseGradeBook 
+    public class RankedGradeBook : BaseGradeBook 
     {
         public new string Name { get; set; }
 
